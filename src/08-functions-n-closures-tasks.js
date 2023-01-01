8dd091b0f922909b515ca1330f6e260d45967a58
Hanna Zalatarevich <a.zoloto@mail.ru>
2023-01-02 00:20:37 +0300

    feat:add

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -180,8 +180,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentCount = startFrom;
+
+  return function t() {
+    const c = currentCount;
+    currentCount += 1;
+    return c;
+  };
 }
 
 
